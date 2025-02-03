26ef5688f81db8a0ec8eed15f2b1c73ff774f3e1
Rajvansh Singh <rajvanshsingh2041@conestogac.on.ca>
2025-02-03 12:02:50 -0500

    Completed Product class implementation

--- ECommerceApplication/Product.cs
+++ ECommerceApplication/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ECommerceApplication
 {
-    internal class Product
+    public class Product
     {
         public int ProdID { get; set; }
         public string ProdName { get; set; }
